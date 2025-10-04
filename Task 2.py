c2e3b480a3e4a958f548b65a448e904fff3b3752
thasmiya786 <thasmiya7864@gmail.com>
2025-10-04 11:26:14 +0530

    intial commit

---
+++ Task 2.py
@@ -0,0 +1,11 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+natural_logarithm = math.log(number)
+sine_value = math.sin(number)
+
+print(f"Square root : {square_root}")
+print(f"Natural logarithm: {natural_logarithm}")
+print(f"Sine value: {sine_value} ")
